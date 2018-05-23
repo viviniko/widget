d5357cdb0d7b65c24736ceb394115b73b94d2ce2
viviniko <viviniko.shop@gmail.com>
2018-05-23 09:48:49 +0800

    :trollface:

--- src/Services/Widget/WidgetServiceImpl.php
+++ src/Services/Widget/WidgetServiceImpl.php
@@ -34,7 +34,9 @@ class WidgetServiceImpl implements WidgetService
     public function getWidgetData($widgetId)
     {
         if ($widget = $this->widgetRepository->find($widgetId)) {
-            return $widget->items->map(function ($item) { return $item->getData(); });
+            return $widget->items->map(function ($item) use ($widget) {
+                return self::getWidgetItemData($widget, $item);
+            });
         }
 
         return false;
@@ -44,28 +46,7 @@ class WidgetServiceImpl implements WidgetService
         $widget = $this->widgetRepository->find($widgetId);
 
         $collection = $widget->items->map(function ($widgetItem) use ($widget) {
-            $data = $widgetItem->getData();
-            if (!property_exists($data, 'parent_id')) {
-                $data->parent_id = 0;
-            }
-            if (!property_exists($data, 'text')) {
-                $data->text = data_get($data, 'title') ?? data_get($data, 'name');
-            }
-            if (!property_exists($data, 'url')) {
-                $data->url = data_get($data, 'link') ?? data_get($data, 'href');
-            }
-            if ($widget->type === WidgetTypes::MENU) {
-                if ($data->text && $data->url) {
-                    $data->text .= "({$data->url})";
-                }
-            }
-            if (!property_exists($data, 'icon')) {
-                if ($icon = data_get($data, 'icon') ?? data_get($data, 'image') ?? data_get($data, 'picture')) {
-                    $data->icon = $icon;
-                }
-            }
-
-            return $data;
+            return self::getWidgetItemData($widget, $widgetItem);
         })->sortBy('sort');
 
         $groupNodes = $collection->groupBy($parentKey);
@@ -77,6 +58,36 @@ class WidgetServiceImpl implements WidgetService
             })->filter(function($item) use ($parentId, $parentKey) {
                 return $item->{$parentKey} == $parentId;
             })->values();
+    }
+
+    protected static function getWidgetItemData($widget, $item)
+    {
+        $data = $item->getData();
+
+        if (!property_exists($data, 'url')) {
+            $data->url = data_get($data, 'link') ?? data_get($data, 'href');
+        }
+        if ($widget->type === WidgetTypes::MENU) {
+            if (!property_exists($data, 'parent_id')) {
+                $data->parent_id = 0;
+            }
+            if (!property_exists($data, 'text')) {
+                $data->text = data_get($data, 'title') ?? data_get($data, 'name');
+            }
+            if ($data->text && $data->url) {
+                $data->text .= "({$data->url})";
+            }
+            if (!property_exists($data, 'icon')) {
+                if ($icon = data_get($data, 'icon') ?? data_get($data, 'image') ?? data_get($data, 'picture')) {
+                    $data->icon = $icon;
+                }
+            }
+        }
+        if ($data->url) {
+            $data->url = url($data->url);
+        }
+
+        return $data;
     }
 
     public function getProductService()
